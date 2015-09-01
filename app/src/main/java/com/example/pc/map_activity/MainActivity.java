8fb005ffe2e0a93049cd4e21b0a7c2611df93f6e
juliocsar13 <jc_redcsar_15@hotmail.com>
2015-09-01 18:11:44 -0500

    implementacion de button davegacion and floatingactionbutton.

--- app/src/main/java/com/example/pc/map_activity/MainActivity.java
+++ app/src/main/java/com/example/pc/map_activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pc.map_activity;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -15,16 +17,29 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import static android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM;
 
 public class MainActivity extends ActionBarActivity {
 
+    private TextView info;
+    FloatingActionButton  fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_notifications);
 
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.layout_logo_bar);
+
+
+
+        /*info =(TextView)findViewById(R.id.infos);*/
+        setupFab();
         /*android.app.ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -45,6 +60,19 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    public void setupFab(){
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,11 +87,30 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.add:
+                //metodoAdd()
+                info.setText("Se presionó Añadir");
+                return true;
+            case R.id.search:
+                //metodoSearch()
+                info.setText("Se presionó Buscar");
+                return true;
+            case R.id.edit:
+                //metodoEdit()
+                info.setText("Se presionó Editar") ;
+                return true;
+            case R.id.delete:
+                //metodoDelete()
+                info.setText("Se presionó Eliminar");
+                return true;
+            case R.id.action_settings:
+                //metodoSettings()
+                info.setText("Se presionó Ajustes");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
